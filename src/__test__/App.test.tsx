a40aedaa6acf41b712538ba8c9d14a5e13cfcf07
pvt12 <pvt12@gcloud.ua.es>
2025-05-13 14:28:50 +0200

    Configuracion para ejecutar test

---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,8 @@
+// Imports
+import { describe, it, expect } from 'vitest';
+// Tests
+describe('Perogrullada', async () => {
+    it('Debe ir sí o sí', async () => {
+        expect(true).toBeTruthy();
+    });
+});
