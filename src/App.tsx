860ca77167fe844a77d4b498e0ae20f0db78f493
pvt12 <pvt12@gcloud.ua.es>
2025-05-13 13:17:59 +0200

    optimizacion

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,29 @@
-import React, { Suspense, useState } from "react";
-import "./App.css";
-import { MenuItem } from "./entities/MenuItem";
-import FoodOrder from "./components/FoodOrder";
-const Foods = React.lazy(() => import("./components/Foods")); // Lazy load
-export const foodItemsContext = React.createContext<MenuItem[]>([]); // Contexto para pasar los items de comida
+import { FC, useState } from "react";
 
-function App() {
-  const [selectedFood, setSelectedFood] = useState<MenuItem | null>(null);
-  const [isChooseFoodPage, setIsChooseFoodPage] = useState(false);
+type ItemListProps = {
+  items: string[];
+};
 
-  const [menuItems, setMenuItems] = useState<MenuItem[]>([
-    {
-      id: 1,
-      name: "Hamburguesa de Pollo",
-      quantity: 40,
-      desc: "Esto es una haburguesa de pollo",
-      price: 12,
-      image: "hamburguesa.jpg",
-    },
-    {
-      id: 2,
-      name: "Pizza de cuatro quesos",
-      quantity: 25,
-      desc: "Esta es la mejor pizza del mundo",
-      price: 15,
-      image: "pizza.jpg",
-    },
-    {
-      id: 3,
-      name: "Ensalada",
-      quantity: 30,
-      desc: "Ensalada con ingredientes naturales",
-      price: 7,
-      image: "ensalada.jpeg",
-    },
-    {
-      id: 4,
-      name: "Bocadillo",
-      quantity: 35,
-      desc: "El bocata de la tía Paca",
-      price: 5,
-      image: "bocadillo.jpg",
-    },
-  ]);
-
+export const ItemList: FC<ItemListProps> = ({ items }) => {
   return (
-    <foodItemsContext.Provider value={menuItems}>
-      <div className="App">
-        {/* Si NO hay comida seleccionada */}
-        {!selectedFood ? (
-          <>
-            <button
-              className="toggleButton"
-              onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
-            >
-              {isChooseFoodPage ? "Disponibilidad" : "Pedir Comida"}
-            </button>
-            <h3 className="title">Comida Rápida Online</h3>
-            {!isChooseFoodPage ? (
-              <>
-                <h4 className="subTitle">Menús</h4>
-                <ul className="ulApp">
-                  {menuItems.map((item) => (
-                    <li key={item.id} className="liApp">
-                      <p>{item.name}</p>
-                      <p>#{item.quantity}</p>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            ) : (
-              <>
-                <Suspense fallback={<div>Cargando platos...</div>}>
-                  <Foods foodItems={menuItems} onFoodSelected={setSelectedFood} />
-                </Suspense>
-              </>
-            )}
-          </>
-        ) : (
-          // Si hay comida seleccionada
-          <FoodOrder
-            food={selectedFood}
-            onReturnToMenu={() => setSelectedFood(null)}
-          />
-        )}
-      </div>
-    </foodItemsContext.Provider>
+    <div>
+      {items.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
   );
-}
+};
 
-export default App;
+export const App: FC = () => {
+  const [count, setCount] = useState<number>(0);
+  const [items, _setItems] = useState<string[]>(
+    Array.from({ length: 1000 }, (_, i) => `Item ${i}`)
+  );
+  return (
+    <div>
+      <h1>Profiling en React</h1>
+      <button onClick={() => setCount(count + 1)}>Incrementar ({count})</button>
+      <ItemList items={items} />
+    </div>
+  );
+};
