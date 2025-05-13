6129a6afaa94fd3ae23551999ac97c090ba6bb6d
pvt12 <pvt12@gcloud.ua.es>
2025-05-13 18:42:04 +0200

    Ejemplo test clase (hasta diapo 305)

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,49 @@
-import React, { Suspense, useState } from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { MenuItem } from "./entities/MenuItem";
-import FoodOrder from "./components/FoodOrder";
-const Foods = React.lazy(() => import("./components/Foods")); // Lazy load
-export const foodItemsContext = React.createContext<MenuItem[]>([]); // Contexto para pasar los items de comida
 
 function App() {
-  const [selectedFood, setSelectedFood] = useState<MenuItem | null>(null);
-  const [isChooseFoodPage, setIsChooseFoodPage] = useState(false);
-
-  const [menuItems, setMenuItems] = useState<MenuItem[]>([
-    {
-      id: 1,
-      name: "Hamburguesa de Pollo",
-      quantity: 40,
-      desc: "Esto es una haburguesa de pollo",
-      price: 12,
-      image: "hamburguesa.jpg",
-    },
-    {
-      id: 2,
-      name: "Pizza de cuatro quesos",
-      quantity: 25,
-      desc: "Esta es la mejor pizza del mundo",
-      price: 15,
-      image: "pizza.jpg",
-    },
-    {
-      id: 3,
-      name: "Ensalada",
-      quantity: 30,
-      desc: "Ensalada con ingredientes naturales",
-      price: 7,
-      image: "ensalada.jpeg",
-    },
-    {
-      id: 4,
-      name: "Bocadillo",
-      quantity: 35,
-      desc: "El bocata de la tía Paca",
-      price: 5,
-      image: "bocadillo.jpg",
-    },
-  ]);
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
 
   return (
-    <foodItemsContext.Provider value={menuItems}>
-      <div className="App">
-        {/* Si NO hay comida seleccionada */}
-        {!selectedFood ? (
-          <>
-            <button
-              className="toggleButton"
-              onClick={() => setIsChooseFoodPage(!isChooseFoodPage)}
-            >
-              {isChooseFoodPage ? "Disponibilidad" : "Pedir Comida"}
-            </button>
-            <h3 className="title">Comida Rápida Online</h3>
-            {!isChooseFoodPage ? (
-              <>
-                <h4 className="subTitle">Menús</h4>
-                <ul className="ulApp">
-                  {menuItems.map((item) => (
-                    <li key={item.id} className="liApp">
-                      <p>{item.name}</p>
-                      <p>#{item.quantity}</p>
-                    </li>
-                  ))}
-                </ul>
-              </>
-            ) : (
-              <>
-                <Suspense fallback={<div>Cargando platos...</div>}>
-                  <Foods foodItems={menuItems} onFoodSelected={setSelectedFood} />
-                </Suspense>
-              </>
-            )}
-          </>
-        ) : (
-          // Si hay comida seleccionada
-          <FoodOrder
-            food={selectedFood}
-            onReturnToMenu={() => setSelectedFood(null)}
-          />
-        )}
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </foodItemsContext.Provider>
-  );
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      <form>
+        <label htmlFor="concat-input">Introduce un texto: </label>
+        <input
+          id="concat-input"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder='Escribe algo..' />
+        <p>
+          Concatenación:{text} {count}
+        </p>
+      </form>
+
+    </div>
+  )
+
 }
 
 export default App;
--- src/__test__/App.test.tsx
+++ src/__test__/App.test.tsx
@@ -1,8 +1,87 @@
 // Imports
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, afterEach } from 'vitest';
+import { cleanup, fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+
+// To Test
+import App from '../App';
 // Tests
-describe('Perogrullada', async () => {
-    it('Debe ir sí o sí', async () => {
-        expect(true).toBeTruthy();
+describe('Renders main page correctly', async () => {
+
+    /*** Reseteamos todos los renderizadores tras cada test */
+    afterEach(() => {
+        cleanup();
     });
+
+    // TEST 1
+    it('Should render the page correctly', async () => {
+        render(<App />);
+        const h1 = await screen.queryByText('Vite + React');
+        expect(h1).not.toBeNull();
+    });
+
+    // TEST 2
+    it('Should show the button count set to 0', async () => {
+        // Inicialización
+        await render(<App />);
+        const button = await screen.queryByText('count is 0');
+        // Comprobaciones
+        expect(button).not.toBeNull();
+    });
+
+    // TEST 3
+    it('Should show the button count set to 3', async () => {
+        // Inicialización
+        await render(<App />);
+        const button = await screen.queryByText('count is 0');
+        // Comprobaciones previas
+        expect(button).not.toBeNull();
+        // Acciones
+        fireEvent.click(button as HTMLElement);
+        fireEvent.click(button as HTMLElement);
+        fireEvent.click(button as HTMLElement);
+        // Comprobaciones posteriores
+        expect(button?.innerHTML).toBe('count is 3');
+    });
+
+    // TEST 4
+    it('Should show the button count set to 3', async () => {
+        const user = userEvent.setup();
+        // Inicialización
+        await render(<App />);
+        const button = await screen.queryByText('count is 0');
+        // Comprobaciones previas
+        expect(button).not.toBeNull();
+        // Acciones
+        //fireEvent.click(button as HTMLElement);
+        await user.click(button as HTMLElement);
+    });
+
+    // TEST 5
+    it('Should render the page correctly', async () => {
+        // Inicialización
+        render(<App />);
+        const h1 = await screen.queryByText('Vite + React');
+        // Comprobaciones
+        // Antes expect(h1).not.toBeNull();
+        // Con Jest DOM
+        expect(h1).toBeInTheDocument();
+    });
+
+    it('renders two images with the correct URLs', () => {
+        render(<App />)
+
+        // Grab all <img> elements
+        const images = screen.getAllByRole('img')
+        expect(images).toHaveLength(2)
+
+        // Check that each has the correct src
+        // Depending on your build, the actual URLs might differ, so you can do substring checks:
+        expect(images[0]).toHaveAttribute('src', expect.stringContaining('vite.svg'))
+        expect(images[1]).toHaveAttribute('src', expect.stringContaining('react.svg'))
+    });
+
+
+
 });
